ede3816e3505ed584201448a74dafa28afe3a134
Thiago Porto <thiagoports@hotmail.com>
2025-05-29 16:11:10 -0300

    feat: filter and pages

--- motos/views.py
+++ motos/views.py
@@ -10,7 +10,33 @@ class MotoListView(LoginRequiredMixin, ListView):
     model = Moto
     template_name = 'motos/index.html'
     context_object_name = 'motos'
+    paginate_by = 8
     login_url = 'login'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        marca = self.request.GET.get('marca')
+        modelo = self.request.GET.get('modelo')
+        preco_max = self.request.GET.get('preco_max')
+        ano = self.request.GET.get('ano')
+
+        if marca and marca != 'TODAS':
+            queryset = queryset.filter(marca=marca)
+        if modelo:
+            queryset = queryset.filter(modelo__icontains=modelo)
+        if preco_max:
+            queryset = queryset.filter(preco__lte=preco_max)
+        if ano:
+            queryset = queryset.filter(ano=ano)
+
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['marcas'] = Moto._meta.get_field('marca').choices
+        context['valores_get'] = self.request.GET  
+        return context
 
 class SignUpView(View):
     def get(self, request):
