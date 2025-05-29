3bf212d08c6339b3e32cb8a1cbc28eedcc126606
Thiago Porto <thiagoports@hotmail.com>
2025-05-29 11:58:07 -0300

    fix: views (Arthur)

--- motos/views.py
+++ motos/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, View
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Moto
 from .forms import SignUpForm
