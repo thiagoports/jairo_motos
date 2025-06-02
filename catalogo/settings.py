97ad3fc9abbd13519e8b810a5e764f8778119841
Thiago Porto <thiagoports@hotmail.com>
2025-06-02 16:49:43 -0300

    style: index melhorada

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'motos_db',
         'USER': 'postgres',
-        'PASSWORD': 'admin123',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
